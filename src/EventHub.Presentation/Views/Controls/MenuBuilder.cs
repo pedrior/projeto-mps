2c3864ddf718bb24091bf866ae92070ee06092a3
Pedro Júnior <pedrojdev@gmail.com>
2023-05-19 00:10:26 -0300

    use builder design pattern in menu ui control

---
+++ src/EventHub.Presentation/Views/Controls/MenuBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace EventHub.Presentation.Views.Controls;
+
+public sealed class MenuBuilder
+{
+    private readonly Menu menu = new();
+
+    public MenuBuilder AddMenuItem(string title, Action action)
+    {
+        var item = new MenuItem
+        {
+            Title = title,
+            Action = action
+        };
+        
+        menu.AddMenuItem(item);
+        return this;
+    }
+    
+    public Menu Build() => menu;
+}
