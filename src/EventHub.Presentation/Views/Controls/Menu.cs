88c5a146ad81c0dc45eb4467d3c346bb8a83d745
Pedro Júnior <pedrojdev@gmail.com>
2023-05-19 00:10:08 -0300

    add menu ui control

---
+++ src/EventHub.Presentation/Views/Controls/Menu.cs
@@ -0,0 +1,36 @@
+﻿namespace EventHub.Presentation.Views.Controls;
+
+public class Menu
+{
+    private readonly List<MenuItem> items = new();
+
+    public void AddMenuItem(MenuItem item) => items.Add(item);
+    
+    public void Display()
+    {
+        for (var i = 0; i < items.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {items[i].Title}");
+        }
+    }
+
+    public MenuItem GetSelectedMenuItem()
+    {
+        int choice;
+        bool isValidChoice;
+
+        do
+        {
+            Console.Write("Enter your choice: ");
+            var input = Console.ReadLine();
+
+            isValidChoice = int.TryParse(input, out choice) && choice >= 1 && choice <= items.Count;
+            if (!isValidChoice)
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+        } while (!isValidChoice);
+
+        return items[choice - 1];
+    }
+}
---
+++ src/EventHub.Presentation/Views/Controls/MenuItem.cs
@@ -0,0 +1,8 @@
+﻿namespace EventHub.Presentation.Views.Controls;
+
+public sealed class MenuItem
+{
+    public required string Title { get; init; }
+    
+    public required Action Action { get; init; }
+}
